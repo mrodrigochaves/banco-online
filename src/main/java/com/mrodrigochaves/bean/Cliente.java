b6df38e9c0b23e5f25d71441b368b10587c35352
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-05-22 20:59:17 -0300

    comentando Cliente

--- src/main/java/com/mrodrigochaves/bean/Cliente.java
+++ src/main/java/com/mrodrigochaves/bean/Cliente.java
@@ -2,46 +2,48 @@ package com.mrodrigochaves.bean;
 
 public class Cliente {
     
-    private Integer id;
-    private String name;
-    private String lastname;
-    private String login;
-    private String password;
+    private Integer id;         
+    private String name;           
+    private String login;       
+    private String password;    
     
+    // Getter para o atributo "id"
     public Integer getId() {
         return id;
     }
+    
+    // Setter para o atributo "id"
     public void setId(Integer id) {
         this.id = id;
     }
+    
+    // Getter para o atributo "name"
     public String getName() {
         return name;
     }
+    
+    // Setter para o atributo "name"
     public void setName(String name) {
         this.name = name;
     }
-    public String getLastName() {
-        return lastname;
-    }
-    public void setLastName(String lastname) {
-        this.lastname = lastname;
-    }
     
+    // Getter para o atributo "login"
     public String getLogin() {
         return login;
     }
+    
+    // Setter para o atributo "login"
     public void setLogin(String login) {
         this.login = login;
     }
+    
+    // Getter para o atributo "password"
     public String getPassword() {
         return password;
     }
+    
+    // Setter para o atributo "password"
     public void setPassword(String password) {
         this.password = password;
     }
-	
-
-
-    
-    
 }
